d3565704a9bba4d22b290d2722878d17709c5fb6
ShiHunYongZhe <1595144933@qq.com>
2019-10-30 11:45:51 +0800

    1030

--- src/api/index.js
+++ src/api/index.js
@@ -3,133 +3,79 @@
  * @param {*} url 请求路径，默认为空
 */
 import ajax from './ajax'
-const BASE_URL = 'http://cangdu.org:8001' // 这里BASE_URL为''也行
-/**
- * 获取 home 页面食品分类列表
- */
-export const reqCategorys = () => ajax('/index_category')
-/**
- * 获取商家信息(下列请求由mock拦截并返回 不需要代理)
- */
-export const reqShopInfo = () => ajax('/info')
-/**
- * 获取问题答案信息
- */
-export const reqquestion = () => ajax('/question')
-/**
- * 获取商家评价数组
- */
-export const reqShopRatings = () => ajax('/ratings')
-/**
- * 获取商家商品数组
- */
-export const reqShopGoods = () => ajax('/goods')
-/**
- * 获取 msite 商铺列表(根据query参数：经纬度)
- * 将经纬度两个数据作为一个参数对象传入
- * 也可以两个数据分别传入ajax， 然后再放入一个对象参数内， 如下面的手机号接口
- */
-export const reqShops = () => ajax('/shops')
-/**
- * 根据经纬度和关键字搜索商铺列表
- */
-export const reqSearchShop = (geohash, keyword) => ajax(BASE_URL + '/search_shops', {geohash, keyword})
-/**
- * 获取短信验证码
- */
-export const reqSendCode = phone => ajax(BASE_URL + '/sendcode', {phone})
-/**
- * 账号密码登录
- */
-export const reqPwdLogin = ({name, pwd, captcha}) => ajax(BASE_URL + '/login_pwd', {name, pwd, captcha}, 'POST')
-/**
- * 手机号验证码登录
- */
-export const reqSmsLogin = (phone, code) => ajax(BASE_URL + '/login_sms', {phone, code}, 'POST')
-/**
- * 改密码
- */
-export const changePassword = (username, oldpassWord, newpassword, confirmpassword, captchaCode) => ajax('BASE_URL', {username, oldpassWord, newpassword, confirmpassword, captchaCode}, 'POST')
-/**
- * 获取用户信息(根据会话)
- */
-export const reqUserInfo = () => ajax(BASE_URL + '/userinfo')
-/**
- * 请求登出
- */
-export const reqLogout = () => ajax(BASE_URL + '/logout')
-/**
- * 获取订单列表
- */
+const BASE_URL = '' // 这里BASE_URL为''也行
 
+
+// (下列请求由mock拦截并返回 不需要代理)
+
+//获取首页默认地址
+export const cityGuess = () => ajax('/cities/guess')
+//获取首页热门城市
+export const hotcity = () => ajax('/cities/hot')
+//获取首页所有城市
+export const groupcity = () => ajax('/cities/group')
+//获取 home 页面食品分类列表
+export const reqCategorys = () => ajax('/index_category')
+//获取商家信息
+export const reqShopInfo = () => ajax('/info')
+//获取问题答案信息
+export const reqquestion = () => ajax('/question')
+//获取商家评价数组
+export const reqShopRatings = () => ajax('/ratings')
+//获取商家商品数组
+export const reqShopGoods = () => ajax('/goods')
+//获取 msite 商铺列表(实际开发需根据query参数：经纬度)
+export const reqShops = () => ajax('/shops')
+
+
+//根据经纬度和关键字搜索商铺列表
+export const reqSearchShop = (geohash, keyword) => ajax(BASE_URL + '/search_shops', {geohash, keyword})
+//获取短信验证码
+export const reqSendCode = phone => ajax(BASE_URL + '/sendcode', {phone})
+//账号密码登录
+export const reqPwdLogin = ({name, pwd, captcha}) => ajax(BASE_URL + '/login_pwd', {name, pwd, captcha}, 'POST')
+//手机号验证码登录
+export const reqSmsLogin = (phone, code) => ajax(BASE_URL + '/login_sms', {phone, code}, 'POST')
+//改密码
+export const changePassword = (username, oldpassWord, newpassword, confirmpassword, captchaCode) => ajax('BASE_URL', {username, oldpassWord, newpassword, confirmpassword, captchaCode}, 'POST')
+//获取用户信息(根据会话)
+export const reqUserInfo = () => ajax(BASE_URL + '/userinfo')
+//请求登出
+export const reqLogout = () => ajax(BASE_URL + '/logout')
+//获取订单列表
 export const getOrderList = (userId, offset) => ajax('/bos/v2/users/' + userId + '/orders', {
   limit: 10,
   offset
 })
-/**
-*兑换会员卡
-*/
+//兑换会员卡
 export const vipCart = (id, number, password) => ajax('/member/v1/users/' + id + '/delivery_card/physical_card/bind', {
   number,
   password
 }, 'POST')
-/**
- * 获取地址信息(根据经纬度串)
- * 这个接口的经纬度参数是在url路径里的，没有query参数
- */
+
+//获取地址信息(根据经纬度串)这个接口的经纬度参数是在url路径里的，没有query参数
 export const reqAddress = geohash => ajax(`${BASE_URL}/position/${geohash}`)
-/**
- * 获取红包
-*/
-export const getHongbaoNum = id => ajax('/promotion/v2/users/' + id + '/hongbaos?limit=20&offset=0')
-/**
- * 获取过期红包
-*/
-export const getExpired = id => ajax('/promotion/v2/users/' + id + '/expired_hongbaos?limit=20&offset=0')
-/**
- * 兑换红包
-*/
+//获取红包
+export const getHongbaoNum = id => ajax('/v2/users/' + id + '/hongbaos?limit=20&offset=0')
+//获取过期红包
+export const getExpired = id => ajax('/v2/users/' + id + '/expired_hongbaos?limit=20&offset=0')
+//兑换红包
 export const exChangeHongbao = (id, exchangeCode, captchaCode) => ajax('/v1/users/' + id + '/hongbao/exchange', {
   exchangeCode,
   captchaCode
 }, 'POST')
 
-/**
- * 获取当前所在城市
- */
-
+//获取当前所在城市
 // export const currentcity = number => ajax('/v1/cities/' + number)
 
-/**
- * 获取搜索地址
- */
+//获取搜索地址
 // export const searchplace = (cityid, value) => ajax('/v1/pois', {
 //   type: 'search',
 //   city_id: cityid,
 //   keyword: value
 // })
 
-/**
- * 获取首页默认地址
- */
-export const cityGuess = () => ajax('/cities/guess')
-
-/**
- * 获取首页热门城市
- */
-
-export const hotcity = () => ajax('/cities/hot')
-
-/**
- * 获取首页所有城市
- */
-
-export const groupcity = () => ajax('/cities/group')
-
-/**
- * 获取search页面搜索结果
- */
-
+//获取search页面搜索结果
 export const searchRestaurant = (geohash, keyword) => fetch('/v4/restaurants', {
   'extras[]': 'restaurant_activity',
   geohash,
