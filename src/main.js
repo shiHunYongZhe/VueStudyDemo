f0ad7124877772466367700e9144c526e4b5fd38
js <1595144933@qq.com>
2019-10-31 20:45:17 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 import App from './App'
 import router from './router'
 import store from './store'
-
+import Vue from 'vue'
 import fastclick from 'fastclick'
 import vueLazyload from 'vue-lazyload'
 import {Button} from 'mint-ui'
@@ -12,7 +12,7 @@ import {Button} from 'mint-ui'
 // 加载mockServer,根据请求响应固定数据
 import './mock/mockServer'
 
-// 在移动端（微信等）使用 vConsole调试console
+// 在移动端（微信等）使用 vConsole调试console（启动需npm安装vConsole）
 // import Vconsole from 'vconsole'
 // let vConsole = new Vconsole()
 // export default vConsole
@@ -29,7 +29,12 @@ Vue.use(vueLazyload, {
   loading: require('./common/imgs/loading.gif')
 })
 // 关闭vue功能的线上实例提示
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+const isDebug_mode = process.env.NODE_ENV !== 'production'
+Vue.config.debug = isDebug_mode
+Vue.config.devtools = isDebug_mode
+Vue.config.productionTip = isDebug_mode
 
 /* eslint-disable no-new */
 new Vue({
