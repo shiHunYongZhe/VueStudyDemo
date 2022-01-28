303f08062953837c1ce9acecdc7b7b5b107539e8
shiHunYongZhe <1595144933@qq.com>
2022-01-28 14:02:06 +0800

    修改ajax函数

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,68 +1,34 @@
 import { baseUrl } from './env'
+import axios from 'axios'
 // 另外一种封装方法，使用fetch和原生ajax，不需要axios
 export default function ajax(url = '', data = {}, type = 'GET', method = 'fetch') {
-	type = type.toUpperCase();
-  url = baseUrl + url;
-	if (type === 'GET') {
-		let dataStr = ''; //数据拼接字符串
-		Object.keys(data).forEach(key => {
-			dataStr += key + '=' + data[key] + '&';
-		})
-
-		if (dataStr !== '') {
-			dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
-			url = url + '?' + dataStr;
-		}
-	}
-
-	if (window.fetch && method == 'fetch') {
-		let requestConfig = {
-			credentials: 'include',
-			method: type,
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			mode: "cors",
-			cache: "force-cache"
-		}
-
-		if (type == 'POST') {
-			Object.defineProperty(requestConfig, 'body', {
-				value: JSON.stringify(data)
-			})
-		}
-	} else {
-		return new Promise((resolve, reject) => {
-			let requestObj;
-			if (window.XMLHttpRequest) {
-				requestObj = new XMLHttpRequest();
-			} else {
-				requestObj = new ActiveXObject;
-			}
-
-			let sendData = '';
-			if (type == 'POST') {
-				sendData = JSON.stringify(data);
-			}
-
-			requestObj.open(type, url, true);
-			requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			requestObj.send(sendData);
-
-			requestObj.onreadystatechange = () => {
-				if (requestObj.readyState == 4) {
-					if (requestObj.status == 200) {
-						let obj = requestObj.response
-						if (typeof obj !== 'object') {
-							obj = JSON.parse(obj);
-						}
-						resolve(obj)
-					} else {
-						reject(requestObj)
-					}
-				}
-			}
-		})
-	}
+    url = baseUrl + url
+	  // 返回值是个 Promise 对象（异步返回的数据是response.data，而不是response）
+    return new Promise(function (resolve, reject) {
+      // （利用axios）异步执行ajax请求
+      let promise // 这个内部的promise用来保存axios的返回值(promise对象)
+      if (type === 'GET') {
+        let dataStr = '' // 数据拼接字符串，将data连接到url
+        Object.keys(data).forEach(key => {
+          dataStr += key + '=' + data[key] + '&'
+        })
+        if (dataStr !== '') {
+          dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+          url += '?' + dataStr
+        }
+        // 发送 get 请求
+        promise = axios.get(url)
+      } else {
+        // 发送 post 请求
+        promise = axios.post(url, data)
+      }
+      promise.then(response => {
+        // 成功回调resolve()
+        resolve(response.data)
+      })
+        .catch(error => {
+          // 失败回调reject()
+          reject(error)
+        })
+    })
 }
